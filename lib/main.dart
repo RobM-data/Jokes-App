24172f58176c1b4a25eb1a08b884f1056d1f0e5f
Rob <robmartin@tutanota.com>
2026-01-15 20:37:45 +0000

    basic swiping feature

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class _JokeSwipePageState extends State<JokeSwipePage> {
     for (int i = 0; i < 20; i++) {
       _swipeItems.add(
         SwipeItem(
-          content: Text('card $i', style: const TextStyle(fontSize: 100)),
+          content: 'card $i',
           likeAction: () {
             print('liked');
           },
@@ -53,21 +53,44 @@ class _JokeSwipePageState extends State<JokeSwipePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Jokes App')),
-      body: Container(
-        height: MediaQuery.of(context).size.height - kToolbarHeight,
-        child: SwipeCards(
-          matchEngine: _matchEngine!,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              alignment: Alignment.center,
-              color: Colors.blue,
-              child: _swipeItems[index].content as Widget,
-            );
-          },
-          onStackFinished: () {
-            print('out of cards');
-          },
-        ),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              'swipe jokes',
+              style: TextStyle(fontSize: 24)
+            )
+          ),
+      
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.5,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: SwipeCards(
+                matchEngine: _matchEngine!,
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24)
+                    ),
+                    elevation: 8,
+                    child: Center(
+                      child: Text(
+                        _swipeItems[index].content as String,
+                        style: const TextStyle(fontSize: 48)
+                      )
+                    )
+                  );
+                },
+                onStackFinished: () {
+                  debugPrint('out of cards');
+                },
+              ),
+            ),
+          ),
+      
+        ],
       ),
     );
   }
